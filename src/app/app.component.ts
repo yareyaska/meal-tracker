61b6d4d2b742296c69e3880c89030378d6bfd07e
yareyaska <uraharahussein@gmail.com>
2019-07-12 11:54:50 -0400

    modified app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Food} from './food';//imports the nterface of the class food to be used for holding new input data.
+import {Food} from './food';
 import {FilterPipe } from './filter.pipe';
 
 @Component({
@@ -12,29 +12,28 @@ export class AppComponent {
 
 
   public masterFoodsList:Food[]=[
-  	//initial elements in the food meals array
+  
   	new Food("ugali","lunch", 343),
   	new Food("Cereals","Breakfast", 580),
   	new Food("Chapatis","Supper",288),
   	new Food("Pizza","snack",212)
   ];
 
-	// adds food meal items to the list throught the array
 	showFood(newFoodFromChild:Food){
     this.masterFoodsList.push(newFoodFromChild);
   }
-  //editting the a selected meal in the list
-  selectedFood:Food=null; //no food selected
+
+  selectedFood:Food=null; 
 
   deleteFoodButton(deleteMeal:Food){
     var index:number = this.masterFoodsList.indexOf(deleteMeal);
     this.masterFoodsList.splice(index,1);
   }
-  //shows the food to edit when it is called in the
+
   showFoodDetailToEdit(food:Food){
     this.selectedFood =food;
   }
-  // set the food items to unselected
+  
   finishedEditing(){
      this.selectedFood=null;
    }
